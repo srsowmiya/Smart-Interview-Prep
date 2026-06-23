d5ed22b4a898e2abc53d8ee518ec9f456d2a1e66
srsowmiya <sowmiyasundaram1040@gmail.com>
2026-06-23 15:48:27 +0530

    resume analyser implemented

---
+++ Backend/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+
+import resumeRouter from "./routes/resume.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/resume", resumeRouter);
+
+app.listen(5000, () => {
+  console.log("Server Running on Port 5000");
+});
